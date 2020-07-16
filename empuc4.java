91d227a224d23205b0050b139e4a958a65bd3200
parag <adhauparag64@gmail.com>
2020-07-16 21:16:14 +0530

    use case 4 added

---
+++ empuc4.java
@@ -0,0 +1,32 @@
+public class empuc4 {
+  public static final int IS_PART_TIME=1;
+  public static final int IS_FULL_TIME =2;
+  public static final int EMP_RATE_PER_HOUR=20;
+
+
+  public void empWage(){
+      int emphrs=0;
+      int empwage=0;
+
+      double empcheck = Math.floor(Math.random() * 10) % 3;
+
+      switch ((int)empcheck){
+      case IS_FULL_TIME:
+       emphrs=8;
+       break;
+      case IS_PART_TIME:
+       emphrs=4;
+       break;
+      default:
+       emphrs=0;
+      }
+      empwage=emphrs * EMP_RATE_PER_HOUR;
+      System.out.println("EMPWAGE="+empwage);
+      }
+    public static void main(String args[]){
+      empuc4  e=new empuc4 ();
+      e.empWage();
+    }
+ }
+
+
